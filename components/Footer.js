05c27adeab164eab2e97a4f8fd3482b27aabb004
Dzmitry <dmitry.litmanovich@pm.me>
2023-01-29 23:12:36 +0400

    Initial.

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
+import { Link } from 'react-dom';
+
 import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <a className={styles.netlifyLink} href="https://netlify.com" target="_blank">
+          Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> Netlify
+        </a>
       </footer>
     </>
   )
